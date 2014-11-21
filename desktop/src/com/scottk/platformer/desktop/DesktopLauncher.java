f643e3a1643a860061439a227b27dfbac9599db9
Krystal Scott <iluvhugs4evr@gmail.com>
2014-11-21 13:33:07 -0800

    commit 1

---
+++ desktop/src/com/scottk/platformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.scottk.platformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.scottk.platformer.Platformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Platformer(), config);
+	}
+}
