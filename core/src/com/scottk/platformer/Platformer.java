a9e3598e4929f9824aed55984481b0f74db4e93a
Krystal Scott <iluvhugs4evr@gmail.com>
2014-12-11 13:43:29 -0800

    commit 6

--- core/src/com/scottk/platformer/Platformer.java
+++ core/src/com/scottk/platformer/Platformer.java
@@ -2,11 +2,12 @@ package com.scottk.platformer;
 
 
 import com.badlogic.gdx.Game;
+import com.scottk.platformer.view.GameScreen;
 
 public class Platformer extends Game{
 
     @Override
     public void create() {
-
+    setScreen(new GameScreen());
     }
 }
--- core/src/com/scottk/platformer/view/GameScreen.java
+++ core/src/com/scottk/platformer/view/GameScreen.java
@@ -23,7 +23,9 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
+     camera.update();
+        renderer.setView(camera);
+        renderer.render();
     }
 
     @Override
