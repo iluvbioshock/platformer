e47cfb742f3056367240890c9d8343fc3e93d7a6
Krystal Scott <iluvhugs4evr@gmail.com>
2014-12-11 13:56:18 -0800

    commit 9

--- core/src/com/scottk/platformer/view/GameScreen.java
+++ core/src/com/scottk/platformer/view/GameScreen.java
@@ -2,6 +2,7 @@ package com.scottk.platformer.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -27,6 +28,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0.64f, 0.28f, 0.21f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
      camera.update();
         renderer.setView(camera);
         renderer.render();
