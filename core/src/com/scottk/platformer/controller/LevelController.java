ac823032d8ca403369ee056e6ab237fd744e63bf
Krystal Scott <iluvhugs4evr@gmail.com>
2015-02-10 20:23:00 -0800

    commit29

--- core/src/com/scottk/platformer/controller/LevelController.java
+++ core/src/com/scottk/platformer/controller/LevelController.java
@@ -2,12 +2,14 @@ package com.scottk.platformer.controller;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.scottk.platformer.model.Level;
+import com.scottk.platformer.model.Player;
 
 /**
  * Created by Student on 1/26/2015.
@@ -21,12 +23,14 @@ public class LevelController {
     public static Batch spriteBatch;
 
     public static World gameWorld;
+    public static Array<Body> worldbodies;
     private  static Box2DDebugRenderer debugRenderer;
 
     public static void  initializedController(){
         level = new Level("image/map01.tmx");
         renderer = new OrthogonalTiledMapRenderer(level.map, UNIT_SCALE);
         gameWorld = new World(new Vector2(0, -10), true);
+        worldbodies = new Array<Body>();
         debugRenderer = new Box2DDebugRenderer();
 
         spriteBatch =  renderer.getSpriteBatch();
@@ -43,6 +47,20 @@ public class LevelController {
     public static void update( float deltaTime) {
         renderer.setView(CameraController.camera);
         renderer.render();
+        PlayerController.update(deltaTime);
+        gameWorld.step(1/100f, 1, 1);
 
     }
-}
+
+    private static void updateWorldBodies(){
+        worldbodies.clear();
+        gameWorld.getBodies(worldbodies);
+
+        for (Body body = worldbodies) {
+            Player playerBody = (Player)body.getUserData();
+            playerBody.position =  body.getPosition();
+        }
+
+        }
+    }
+
